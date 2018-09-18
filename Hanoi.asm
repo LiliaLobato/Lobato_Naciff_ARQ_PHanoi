ab3964b01da26a84971ad4ee3ffa1748e6d43ac2
LiliaLobato <ie706937@iteso.mx>
2018-09-18 17:30:08 -0500

    recursividad ERROR RA

--- Hanoi.asm
+++ Hanoi.asm
@@ -10,10 +10,12 @@ main:
 	la $s2, 0x10010020  # Loading an address
 	la $s3, 0x10010040  # Loading an address
 	
+	#Inicializar
 	and $s0, $s0, $zero 
-	ori $s0, $s0, 1		# s0 -> n -> numero de Discos
+	ori $s0, $s0, 2		# s0 -> n -> numero de Discos
 	add $s4, $s4, $s0	#Copia de n
 	
+	#Cargar to, aux
 	add $t2, $t2, $s3
 	add $t3, $t3, $s2
 	
@@ -22,23 +24,56 @@ Inicializar:
 	sw  $s4, 0($s1)		# Loading values from stak
 	addi $s1, $s1, 4 	# Increasing stack pointer
 	addi $s4, $s4, -1	# Decrement de valor
-	bne $s4, $zero, Inicializar   # if n < 1
-	addi $s1, $s1, -4 	# Increasing stack pointer
-	add $t1, $t1, $s1
+	bne $s4, $zero, Inicializar   # if n != 0
+	addi $s1, $s1, -4 	# Apuntar ultimo valor
+	add $t1, $t1, $s1	# Cargar from
 	jal Hanoi 		# Calling procedure
 	j Exit
 
-Hanoi:
-	
-	bne $s0, 1, Recursivo   # if n < 1
-	lw $s5, 0($t1)
-	sw $zero, 0($t1)
-	sw $s5, 0($t2)
-	jr $ra  		# Return to the caller
-	j Exit
-
+Hanoi:	
+N1:	bne $s0, 1, Recursivo   # if n < 1
+	lw $s5, 0($t1)		#Guarda valor ultimo
+	sw $zero, 0($t1)	#Elimina de Stack ultimo valor
+	sw $s5, 0($t2)		#Guarda valor en Stack
+	addi $t1, $t1, -4 	# Apuntar ultimo valor
+	addi $t2, $t2, 4 	# Apuntar ultimo valor
+	jr $ra  		# Return to the caller	
+			
 Recursivo:
-
+	add $s4, $zero, $s0	#copia n
+	
+	add $t4, $zero, $t1
+	add $t5, $zero, $t2
+	add $t6, $zero, $t3
+	
+	add $t1, $zero, $t1
+	add $t0, $zero, $t2
+	add $t2, $zero, $t3
+	add $t3, $zero, $t0
+	add $s0, $s0, -1
+	jal Hanoi		#Llamar hanoi con n-1, from=from,to=aux, aux=to
+	
+	add $s0, $zero, $s4
+	add $s0, $s0, -1
+	add $t1, $zero, $t4
+	add $t2, $zero, $t5
+	add $t3, $zero, $t6
+	
+	#Mover
+	lw $s5, 0($t1)		#Guarda valor ultimo
+	sw $zero, 0($t1)	#Elimina de Stack ultimo valor
+	sw $s5, 0($t2)		#Guarda valor en Stack
+	addi $t1, $t1, -4 	# Apuntar ultimo valor
+	addi $t2, $t2, 4 	# Apuntar ultimo valor
+	
+	
+	#Llamar hanoi con n-1, from=aux,to=to, aux=from
+	add $t0, $zero, $t1
+	add $t1, $zero, $t3
+	add $t2, $zero, $t2
+	add $t3, $zero, $t0
+	jal Hanoi	
+	
 Exit: 
 	
 	
