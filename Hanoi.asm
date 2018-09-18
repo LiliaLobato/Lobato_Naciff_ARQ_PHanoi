1beacbc8a52a16ff6862e9eee638cee88e70f607
LiliaLobato <ie706937@iteso.mx>
2018-09-18 16:56:45 -0500

    excepcion

--- Hanoi.asm
+++ Hanoi.asm
@@ -11,23 +11,30 @@ main:
 	la $s3, 0x10010040  # Loading an address
 	
 	and $s0, $s0, $zero 
-	ori $s0, $s0, 4		# s0 -> n -> numero de Discos
+	ori $s0, $s0, 1		# s0 -> n -> numero de Discos
 	add $s4, $s4, $s0	#Copia de n
-	add $sp, $zero, $s1   	# Direccion cargado Discos
+	
+	add $t2, $t2, $s3
+	add $t3, $t3, $s2
 	
 	#Inicializando Torre A 
 Inicializar:
-	sw  $s4, 0($sp)		# Loading values from stak
-	addi $sp, $sp, 4 	# Increasing stack pointer
+	sw  $s4, 0($s1)		# Loading values from stak
+	addi $s1, $s1, 4 	# Increasing stack pointer
 	addi $s4, $s4, -1	# Decrement de valor
 	bne $s4, $zero, Inicializar   # if n < 1
+	addi $s1, $s1, -4 	# Increasing stack pointer
+	add $t1, $t1, $s1
 	jal Hanoi 		# Calling procedure
 	j Exit
 
 Hanoi:
 	
 	bne $s0, 1, Recursivo   # if n < 1
-				#Mueve
+	lw $s5, 0($t1)
+	sw $zero, 0($t1)
+	sw $s5, 0($t2)
+	jr $ra  		# Return to the caller
 	j Exit
 
 Recursivo:
