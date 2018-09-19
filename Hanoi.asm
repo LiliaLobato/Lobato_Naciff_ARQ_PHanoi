1fef3fc02275c93c4a2f75c06b03a618fa7563e8
LiliaLobato <ie706937@iteso.mx>
2018-09-18 20:01:26 -0500

    Error 3+

--- Hanoi.asm
+++ Hanoi.asm
@@ -6,13 +6,14 @@
 .text
 main:
 	# Almacena la direccion de cada Arreglo
-	la $s1, 0x10010000  # Loading an address
-	la $s2, 0x10010020  # Loading an address
-	la $s3, 0x10010040  # Loading an address
+	la $s1, 0x10010000  # Loading fila A
+	la $s2, 0x10010020  # Loading fila B
+	la $s3, 0x10010040  # Loading fila C
+	la $s6, 0x10010060  # Loading return address
 	
 	#Inicializar
 	and $s0, $s0, $zero 
-	ori $s0, $s0, 2		# s0 -> n -> numero de Discos
+	ori $s0, $s0, 3		# s0 -> n -> numero de Discos
 	add $s4, $s4, $s0	#Copia de n
 	
 	#Cargar to, aux
@@ -28,18 +29,22 @@ Inicializar:
 	addi $s1, $s1, -4 	# Apuntar ultimo valor
 	add $t1, $t1, $s1	# Cargar from
 	jal Hanoi 		# Calling procedure
+zero:	add $zero,$zero,$zero
 	j Exit
 
 Hanoi:	
+	
 N1:	bne $s0, 1, Recursivo   # if n < 1
-	lw $s5, 0($t1)		#Guarda valor ultimo
-	sw $zero, 0($t1)	#Elimina de Stack ultimo valor
-	sw $s5, 0($t2)		#Guarda valor en Stack
+	lw $s5, 0($t1)		# Guarda valor ultimo
+	sw $zero, 0($t1)	# Elimina de Stack ultimo valor
+	sw $s5, 0($t2)		# Guarda valor en Stack
 	addi $t1, $t1, -4 	# Apuntar ultimo valor
 	addi $t2, $t2, 4 	# Apuntar ultimo valor
+	
 	jr $ra  		# Return to the caller	
 			
 Recursivo:
+	
 	add $s4, $zero, $s0	#copia n
 	
 	add $t4, $zero, $t1
@@ -60,6 +65,7 @@ Recursivo:
 	add $t3, $zero, $t6
 	
 	#Mover
+	addi $t1, $t1, -4 	# Apuntar ultimo valor
 	lw $s5, 0($t1)		#Guarda valor ultimo
 	sw $zero, 0($t1)	#Elimina de Stack ultimo valor
 	sw $s5, 0($t2)		#Guarda valor en Stack
@@ -72,6 +78,7 @@ Recursivo:
 	add $t1, $zero, $t3
 	add $t2, $zero, $t2
 	add $t3, $zero, $t0
+	
 	jal Hanoi	
 	
 Exit: 
