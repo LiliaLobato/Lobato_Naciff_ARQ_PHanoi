152bd3465168fd27b4f0066bb2c117bffcf6a6ab
LiliaLobato <ie706937@iteso.mx>
2018-09-21 00:49:08 -0500

    Final

--- Hanoi2.asm
+++ Hanoi2.asm
@@ -1,34 +1,53 @@
+#########################################
+#           TORRES DE HANOI             #
+# Lilia Arceli Lobato Martínez ie706937 #
+# Jorge Karim Naciff Maldonnat ie	#
+#########################################
 
 .data
 #Apartado de memoria para torres
-A: .word 0 0 0 0 0 0 0 0 #0 0 0 0 0 0 0
-B: .word 0 0 0 0 0 0 0 0 #0 0 0 0 0 0 0
-C: .word 0 0 0 0 0 0 0 0 #0 0 0 0 0 0 0
+A: .word 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0
+B: .word 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0
+C: .word 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0
 
 .text
 main:
 	#Carga de direccion de torres
-	la $a1, A		#Carga direccion to
-	la $a2, C		#Carga direccion from
-	la $a3, B		#Carga direccion aux
-	la $t6, A		#Carga direccion para llenado inicial
-	#Num. Discos
-	add $a0,$zero, 8
+	#la $a1, A		#Carga direccion to
+	addi $a1, $zero, 0x1001
+	sll $a1, $a1, 16		#Carga direccion to
+	ori $a1, $a1, 0x0000
+	#la $t6, A		#Carga direccion para llenado inicial
+	addi $t6, $zero, 0x1001
+	sll $t6, $t6, 16		#Carga direccion to
+	ori $t6, $t6, 0x0000
 	
-	#Copia Num. Discos
-	add $s2,$zero, $a0
+	#la $a2, C		#Carga direccion from
+	addi $a2, $zero, 0x1001
+	sll $a2, $a2, 16		#Carga direccion from
+	ori $a2, $a2, 0x0080
+	
+	#la $a3, B		#Carga direccion aux
+	addi $a3, $zero, 0x1001
+	sll $a3, $a3, 16		#Carga direccion aux
+	ori $a3, $a3, 0x0040
+	
+	#Discos
+	add $a0,$zero, 3	#Num. Discos
+	add $s2,$zero, $a0	#Copia Num. Discos
 	
 	#Apuntador a tope torre
-	sll $s1, $a0, 2
-	add $a1, $a1, $s1
-	addi $a1, $a1, -4
+	sll $s1, $a0, 2		#Al darle un Shift Left Logical por 2 a un valor
+				#es equivalente a multiplicar por 4
+	add $a1, $a1, $s1	#La dirección de la última posición de la torre to se guarda
+	addi $a1, $a1, -4	#Se decrementa para que apunte al último valor guardado
 	
 	#Llenado de Torre 1
 for:	
-	sw $s2, ($t6)
-	addi $t6, $t6, 4
-	addi $s2, $s2, -1
-	bne $s2, 0, for
+	sw $s2, ($t6)		#Se guarda el num. actual de disco en A
+	addi $t6, $t6, 4	#se avanza en posición torre A
+	addi $s2, $s2, -1	#Se decrementa el valor de disco
+	bne $s2, 0, for		#Si discos!=0 -> for
 endFor:	
 	jal hanoi
 	j exit
@@ -59,7 +78,7 @@ nouno:
 	
 	#Cambio apuntadores 2
 	add $t0, $zero, $a1
-	addi $a1, $a2, 4 	#Drecrementa tamaño aux y cambio from -> to	
+	sub $a1, $a2, 4 	#Drecrementa tamaño aux y cambio from -> to	
 	addi $a2, $a3, 4	#Drecrementa tamaño aux y cambio to -> aux
 	addi $a3, $t0, 0 	#Cambio aux -> from
 	
@@ -86,7 +105,6 @@ uno:
 	addi $a1, $a1, -4	#Decrementa el tamaño de Torre from
 	addi $a2, $a2, 4	#Incrementa tamaña torre to
 	
-	jr $ra
-	
+	jr $ra	
 
 exit:
